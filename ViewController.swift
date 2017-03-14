bd422b24815485895d3d2074608f412ca79e0953
Daya <dayakevin5@gmail.com>
2017-03-14 16:16:59 +0530

    Added new comment line inside viewDidLoad()

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3CD
+//
+//  Created by Admin on 1/30/17.
+//  Copyright © 2017 HCL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Hey I have added a new line of comment.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
